bd5d3dbf25c08cd9441174f441f1bac539de6aad
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-10 20:19:36 +0300

    add delete task emit-event Task.vue

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
   },
   methods:{
     deleteTask(id){
-       console.log('task -> ',id)
+       this.tasks = this.tasks.filter((task)=> task.id != id )
     }
   },
   created(){
