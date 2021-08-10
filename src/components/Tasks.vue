f5a9daa9a5ee483e63bea75677c5957b483b55c3
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-10 19:47:15 +0300

    add Task.vue

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,18 @@
 <template>
 	<div :key="task.id" v-for="task in tasks">
-		<h3>{{task.text}}</h3>
+       <Task :task="task"/>
 	</div>
 </template>
 
 <script>
+	import Task from './Task.vue'
 	export default {
 		name:'Tasks',
 	    props:{
 	    	tasks:Array
+	    },
+	    components:{
+	    	Task
 	    }
 	}
 </script>
