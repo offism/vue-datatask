c50714aea5644871ca79184d3fb60accf76d7e07
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-10 20:02:16 +0300

    refresh Task.vue

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,12 +1,38 @@
 <template>
-	<h3>{{task.text}}</h3>
+	<div :class="[task.reminder ? 'reminder' : '','task']">
+		<div class="head-body">
+		<h3>{{task.text}}</h3>
+		<i class="fas fa-times" style="color: red;"></i>
+		</div>
+		<small>{{task.day}}</small>
+	</div>
 </template>
 
 <script>
 	export default {
 		name:'Task',
-	    props:{
-	    	task:Object
-	    }
+		props:{
+			task:Object
+		}
 	}
 </script>
+
+<style scoped>
+small{
+	color: #888;
+	}
+	.task{
+		background:#f4f4f4;
+		margin: 5px;
+		padding: 10px 20px;
+		cursor: pointer;
+	}
+	.task.reminder{
+		border-left: 5px solid green;
+	}
+	.task .head-body{
+		display: flex;
+		align-items: center;
+		justify-content: space-between ;
+	}
+</style>
