b897f3a2c8ada68a3bf934667f5101c8fb798fb4
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-10 19:17:51 +0300

    '3c'

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+	<header>
+		<h1>{{title}}</h1>
+		<Button text="Add Button" color="green"/>
+	</header>
+</template>
+
+<script>
+	import Button from './Button.vue'
+	export default {
+		name:'Header',
+		components:{
+			Button
+		},
+		props:{
+			title: String
+		}
+	}
+</script>
+
+<style>
+	header{
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		margin-bottom: 20px;
+	}
+</style>
